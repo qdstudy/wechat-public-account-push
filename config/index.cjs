2ffc4b76202319ae2948f92438df166ebe21d5f8
前度 <56963129+qdstudy@users.noreply.github.com>
2025-03-12 21:02:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oiQFc6tkHIh6wilQ1YXzanYP21bg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'clUoX8fiw861tWqTTtbE50dcF_TxbyaZwJi96yZu_ZE',
+      useTemplateId: 'wRXoolV57lEVTigDkeUleqQ21fHVBMYqMXLmN6jNMdc',
       // 所在省份
       province: "江苏",
       // 所在城市
@@ -56,43 +56,43 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
-    {
-       // 想要发送的人的名字
-       name: 'me',
-       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-       id: 'oiQFc6tkHIh6wilQ1YXzanYP21bg',
-       // 使用微信测试号：你想对他发送的模板消息的模板ID
-       useTemplateId: 'clUoX8fiw861tWqTTtbE50dcF_TxbyaZwJi96yZu_ZE',
-       // 所在省份
-       province: "江苏",
-       // 所在城市
-       city: "苏州",
-       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-       // horoscopeDateType: '今日',
-       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-       // openUrl: "https://wangxinleo.cn",
-       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-       // horoscopeDate: '10-02',
-       festivals: [
-         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-         {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-         {"type": "*生日", "name": "老爸", "year": "1976", "date": "03-09", isShowAge: true},
-         {"type": "*生日", "name": "老妈", "year": "1977", "date": "11-17", isShowAge: true},
-         {"type": "*生日", "name": "老妹", "year": "2005", "date": "11-22", isShowAge: true},
-         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-         {"type": "节日", "name": "结婚纪念日", "year": "1996", "date": "09-01"},
-         {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-       ],
-       festivals_limit: 6,
-       // 我们在一起已经有xxxx天了的配置
-       customizedDateList: [
-         // 在一起的日子
-         { keyword: 'love_day', date: '2022-09-08' },
-         // 结婚纪念日
-         { keyword: 'marry_day', date: '2022-09-09' },
-       ],
+    // {
+    //    // 想要发送的人的名字
+    //    name: 'me',
+    //    // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+    //    id: 'oiQFc6tkHIh6wilQ1YXzanYP21bg',
+    //    // 使用微信测试号：你想对他发送的模板消息的模板ID
+    //    useTemplateId: 'clUoX8fiw861tWqTTtbE50dcF_TxbyaZwJi96yZu_ZE',
+    //    // 所在省份
+    //    province: "江苏",
+    //    // 所在城市
+    //    city: "苏州",
+    //    // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+    //    // horoscopeDateType: '今日',
+    //    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+    //    // openUrl: "https://wangxinleo.cn",
+    //    // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+    //    // horoscopeDate: '10-02',
+    //    festivals: [
+    //      // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    //      {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
+    //      {"type": "*生日", "name": "老爸", "year": "1976", "date": "03-09", isShowAge: true},
+    //      {"type": "*生日", "name": "老妈", "year": "1977", "date": "11-17", isShowAge: true},
+    //      {"type": "*生日", "name": "老妹", "year": "2005", "date": "11-22", isShowAge: true},
+    //      // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+    //      {"type": "节日", "name": "结婚纪念日", "year": "1996", "date": "09-01"},
+    //      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    //    ],
+    //    festivals_limit: 6,
+    //    // 我们在一起已经有xxxx天了的配置
+    //    customizedDateList: [
+    //      // 在一起的日子
+    //      { keyword: 'love_day', date: '2022-09-08' },
+    //      // 结婚纪念日
+    //      { keyword: 'marry_day', date: '2022-09-09' },
+    //    ],
 
-    },
+    // },
   ],
 
 
