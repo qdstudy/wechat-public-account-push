10a6105a3d90661b8a89b293a1271437c28c99e0
前度 <56963129+qdstudy@users.noreply.github.com>
2025-04-18 09:26:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -62,7 +62,7 @@ const USER_CONFIG = {
        // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
        id: 'oiQFc6tkHIh6wilQ1YXzanYP21bg',
        // 使用微信测试号：你想对他发送的模板消息的模板ID
-       useTemplateId: 'nmEprXIS5LiX99tByqKQXSHesIHJaaU_RCHOB5IdhKc',
+       useTemplateId: 'UnbxIU2rtdFIXXJ3HJb1iaribaXl7rLIjoLwY_34N0c',
        // 所在省份
        province: "江苏",
        // 所在城市
